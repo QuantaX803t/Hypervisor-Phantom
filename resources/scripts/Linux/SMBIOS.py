8ecba39894eca9fbfeeaf3b778fa3b31536c6166
Scrut1ny <53458032+Scrut1ny@users.noreply.github.com>
2026-05-22 06:49:08 -0400

    Update SMBIOS.py

--- resources/scripts/Linux/SMBIOS.py
+++ resources/scripts/Linux/SMBIOS.py
@@ -13,12 +13,12 @@ def get_bytes(path):
 # 1. Table concatenation
 data = get_bytes("/sys/firmware/dmi/tables/smbios_entry_point") + get_bytes("/sys/firmware/dmi/tables/DMI")
 
-# 2. Overwrite UUID bytes with 0xFF
+# 2. Overwrite UUID bytes
 if (u_txt := get_bytes("/sys/class/dmi/id/product_uuid").strip()):
     b = bytes.fromhex(u_txt.decode().replace("-", ""))
     data = data.replace(b[3::-1] + b[5:3:-1] + b[7:5:-1] + b[8:], b"\xFF" * 16)
 
-# 3. Overwrite serial number strings with 0x00
+# 3. Overwrite SN strings
 for name in ("product_serial", "board_serial", "chassis_serial"):
     if val := get_bytes(f"/sys/class/dmi/id/{name}").strip():
         data = data.replace(val, b"To be filled by O.E.M.")
