8db290712d83e859ce81b6bcb572df620221c39f
Scrut1ny <53458032+Scrut1ny@users.noreply.github.com>
2026-05-24 12:57:40 -0400

    Adjusted variable names

--- resources/scripts/Linux/SMBIOS.py
+++ resources/scripts/Linux/SMBIOS.py
@@ -14,19 +14,19 @@ def get_bytes(path):
 data = get_bytes("/sys/firmware/dmi/tables/smbios_entry_point") + get_bytes("/sys/firmware/dmi/tables/DMI")
 
 # 2. Overwrite UUID bytes
-if (u_txt := get_bytes("/sys/class/dmi/id/product_uuid").strip()):
-    b = bytes.fromhex(u_txt.decode().replace("-", ""))
+if (uuid := get_bytes("/sys/class/dmi/id/product_uuid").strip()):
+    b = bytes.fromhex(uuid.decode().replace("-", ""))
     data = data.replace(b[3::-1] + b[5:3:-1] + b[7:5:-1] + b[8:], b"\xFF" * 16)
 
 # 3. Overwrite SN strings
-for name in ("board_serial", "chassis_serial", "product_serial"):
-    if val := get_bytes(f"/sys/class/dmi/id/{name}").strip():
+for attr in ("board_serial", "chassis_serial", "product_serial"):
+    if val := get_bytes(f"/sys/class/dmi/id/{attr}").strip():
         data = data.replace(val, b"To be filled by O.E.M.")
 
-for p in Path("/sys/firmware/dmi/entries/").glob("17-*/raw"):
-    for s in get_bytes(p)[2:].split(b'\x00'):
-        if len(s) == 8 and s.isalnum():
-            data = data.replace(s.lower(), b"00000000").replace(s.upper(), b"00000000")
+for entry in Path("/sys/firmware/dmi/entries/").glob("17-*/raw"):
+    for token in get_bytes(entry)[2:].split(b'\x00'):
+        if len(token) == 8 and token.isalnum():
+            data = data.replace(token.lower(), b"00000000").replace(token.upper(), b"00000000")
 
 if data:
     Path(args.output).write_bytes(data)
