60c984ee09137b07bd165831b4d88cc197f83a57
Scrut1ny <53458032+Scrut1ny@users.noreply.github.com>
2026-05-25 01:08:19 -0400

    utilize python's uuid module

--- resources/scripts/Linux/SMBIOS.py
+++ resources/scripts/Linux/SMBIOS.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+
 from pathlib import Path
+from uuid import UUID
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -7,26 +9,25 @@ parser.add_argument("-o", "--output", default="smbios.bin")
 args = parser.parse_args()
 
 def get_bytes(path):
-    try: return Path(path).read_bytes()
-    except OSError: return b""
+	try: return Path(path).read_bytes()
+	except OSError: return b""
 
 # 1. Table concatenation
 data = get_bytes("/sys/firmware/dmi/tables/smbios_entry_point") + get_bytes("/sys/firmware/dmi/tables/DMI")
 
 # 2. Overwrite UUID bytes
-if (uuid := get_bytes("/sys/class/dmi/id/product_uuid").strip()):
-    b = bytes.fromhex(uuid.decode().replace("-", ""))
-    data = data.replace(b[3::-1] + b[5:3:-1] + b[7:5:-1] + b[8:], b"\xFF" * 16)
+if raw := get_bytes("/sys/class/dmi/id/product_uuid").strip():
+	data = data.replace(UUID(raw.decode()).bytes_le, b"\xFF" * 16)
 
 # 3. Overwrite SN strings
 for attr in ("board_serial", "chassis_serial", "product_serial"):
-    if val := get_bytes(f"/sys/class/dmi/id/{attr}").strip():
-        data = data.replace(val, b"To be filled by O.E.M.")
+	if val := get_bytes(f"/sys/class/dmi/id/{attr}").strip():
+		data = data.replace(val, b"To be filled by O.E.M.")
 
 for entry in Path("/sys/firmware/dmi/entries/").glob("17-*/raw"):
-    for token in get_bytes(entry)[2:].split(b'\x00'):
-        if len(token) == 8 and token.isalnum():
-            data = data.replace(token.lower(), b"00000000").replace(token.upper(), b"00000000")
+	for token in get_bytes(entry)[2:].split(b'\x00'):
+		if len(token) == 8 and token.isalnum():
+			data = data.replace(token.lower(), b"00000000").replace(token.upper(), b"00000000")
 
 if data:
-    Path(args.output).write_bytes(data)
+	Path(args.output).write_bytes(data)
